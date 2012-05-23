31bcbb1952c660e2460261aeb25b8d86772615b6
Jens Kraemer <jk@jkraemer.net>
2012-05-23 15:07:08 +0200

    Merge

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -7,13 +7,13 @@ class NoticesController < ActionController::Base
   before_filter :check_enabled
   before_filter :find_or_create_custom_fields
 
-  unloadable  
+  unloadable
 
   TRACE_FILTERS = [
     /^On\sline\s#\d+\sof/,
     /^\d+:/
   ]
-  
+
   def index_v2
     logger.debug {"received v2 request:\n#{@notice.inspect}\nwith redmine_params:\n#{@redmine_params.inspect}"}
     create_or_update_issue @redmine_params, @notice
@@ -25,7 +25,7 @@ class NoticesController < ActionController::Base
     logger.debug {"transformed arguments:\n#{notice.inspect}"}
     create_or_update_issue @redmine_params, notice
   end
-  
+
   private
 
   def create_or_update_issue(redmine_params, notice)
@@ -57,10 +57,10 @@ class NoticesController < ActionController::Base
     end
 
     issue = Issue.find_by_subject_and_project_id_and_tracker_id_and_author_id(subject, project.id, tracker.id, author.id)
-    issue ||= Issue.new(:subject => subject, :project_id => project.id, :tracker_id => tracker.id, :author_id => author.id)
-    issue_is_new = issue.new_record?
+    if issue.nil?
+      # new issue
+      issue = Issue.new(:subject => subject, :project_id => project.id, :tracker_id => tracker.id, :author_id => author.id)
 
-    if issue_is_new
       # set standard redmine issue fields
       issue.category = IssueCategory.find_by_name(redmine_params[:category]) unless redmine_params[:category].blank?
       issue.assigned_to = (User.find_by_login(redmine_params[:assigned_to]) || Group.find_by_lastname(redmine_params[:assigned_to])) unless redmine_params[:assigned_to].blank?
@@ -75,44 +75,47 @@ class NoticesController < ActionController::Base
       unless redmine_params[:environment].blank?
         issue.custom_values.build(:custom_field => @environment_field, :value => redmine_params[:environment])
       end
+      issue.skip_notification = true
+      issue.save!
     end
-    issue.skip_notification = true
-    issue.save!
 
     # increment occurences custom field
-    value = issue.custom_value_for(@occurences_field) || issue.custom_values.build(:custom_field => @occurences_field, :value => 0)
-    value.value = (value.value.to_i + 1).to_s
-    value.save!
-
-    # update journal
-    text = "h4. Error message\n\n<pre>#{error_message}</pre>"
-    text << "\n\nh4. Filtered backtrace\n\n<pre>#{format_backtrace(filtered_backtrace)}</pre>" unless filtered_backtrace.blank?
-    text << "\n\nh4. Request\n\n<pre>#{format_hash notice['request']}</pre>" unless notice['request'].blank?
-    text << "\n\nh4. Session\n\n<pre>#{format_hash notice['session']}</pre>" unless notice['session'].blank?
-    unless (env = (notice['server_environment'] || notice['environment'])).blank?
-      text << "\n\nh4. Environment\n\n<pre>#{format_hash env}</pre>"
-    end
-    text << "\n\nh4. Full backtrace\n\n<pre>#{format_backtrace backtrace}</pre>" unless backtrace.blank?
-    journal = issue.init_journal author, text
-
-    # reopen issue if needed
-    if issue.status.blank? or issue.status.is_closed?                                                                                                        
-      issue.status = IssueStatus.find(:first, :conditions => {:is_default => true}, :order => 'position ASC')
-    end
-
-    issue.save!
-
-    if issue_is_new
-      Mailer.deliver_issue_add(issue) if Setting.notified_events.include?('issue_added')
-      Redmine::Hook.call_hook(:controller_issues_new_after_save, { :issue => issue, :project => project })
+    if value = issue.custom_value_for(@occurences_field)
+      value.update_attribute :value, (value.value.to_i + 1).to_s
     else
-      Mailer.deliver_issue_edit(journal) if Setting.notified_events.include?('issue_updated')
-      if now = Time.new.to_i and @@hook_quarantine < now
-        Redmine::Hook.call_hook(:controller_issues_edit_after_save, { :issue => issue, :journal => journal })
-        @@hook_quarantine = now + redmine_params[:hook_quarantine].to_i
+      issue.custom_values.create!(:value => 1, :custom_field => @occurences_field)
+    end
+
+    # create the journal entry, update issue attributes
+    retried_once = false # we retry once in case of a StaleObjectError
+    begin
+      # update journal
+      text = "h4. Error message\n\n<pre>#{error_message}</pre>"
+      text << "\n\nh4. Filtered backtrace\n\n<pre>#{format_backtrace(filtered_backtrace)}</pre>" unless filtered_backtrace.blank?
+      text << "\n\nh4. Request\n\n<pre>#{format_hash notice['request']}</pre>" unless notice['request'].blank?
+      text << "\n\nh4. Session\n\n<pre>#{format_hash notice['session']}</pre>" unless notice['session'].blank?
+      unless (env = (notice['server_environment'] || notice['environment'])).blank?
+        text << "\n\nh4. Environment\n\n<pre>#{format_hash env}</pre>"
+      end
+      text << "\n\nh4. Full backtrace\n\n<pre>#{format_backtrace backtrace}</pre>" unless backtrace.blank?
+      journal = issue.init_journal author, text
+
+
+      # reopen issue if needed
+      if issue.status.blank? or issue.status.is_closed?
+        issue.status = IssueStatus.find(:first, :conditions => {:is_default => true}, :order => 'position ASC')
+      end
+
+      issue.save!
+    rescue ActiveRecord::StaleObjectError
+      if retried_once
+        Rails.logger.error "airbrake server: failed to update issue #{issue.id} for the second time, giving up."
+      else
+        retried_once = true
+        issue = Issue.find issue.id
+        retry
       end
     end
-    
     render :status => 200, :text => "Received bug report.\n<error-id>#{issue.id}</error-id>"
   end
 
@@ -149,15 +152,15 @@ class NoticesController < ActionController::Base
       end.flatten.compact.uniq.any?
     end
   end
-  
+
   def format_backtrace(lines)
     lines.map{ |line| "#{line['file']}:#{line['number']}#{":in #{line['method']}" if line['method']}" }.join("\n")
   end
-  
+
   def cleanup_path(path)
     path.gsub(/\[(PROJECT|RAILS)_ROOT\]\//,'')
   end
-    
+
   # before_filter, checks api key and parses request
   def check_enabled
     User.current = nil
@@ -167,7 +170,7 @@ class NoticesController < ActionController::Base
       false
     end
   end
-  
+
   def parse_request
     logger.debug { "hoptoad error notification:\n#{request.raw_post}" }
     case params[:action]
@@ -191,7 +194,7 @@ class NoticesController < ActionController::Base
     @api_key = @redmine_params[:api_key]
     true
   end
-  
+
   # make sure the custom fields exist, and load them for further usage
   def find_or_create_custom_fields
     @error_class_field = IssueCustomField.find_or_initialize_by_name('Error class')
@@ -224,19 +227,19 @@ class NoticesController < ActionController::Base
       @repository_root_field.save(false)
     end
   end
-  
+
   # make sure that custom fields are associated to this project and tracker
   def ensure_tracker_has_fields(tracker)
     tracker.custom_fields << @error_class_field unless tracker.custom_fields.include?(@error_class_field)
     tracker.custom_fields << @occurences_field unless tracker.custom_fields.include?(@occurences_field)
     tracker.custom_fields << @environment_field unless tracker.custom_fields.include?(@environment_field)
   end
-  
+
   # make sure that custom fields are associated to this project and tracker
   def ensure_project_has_fields(project)
     project.issue_custom_fields << @error_class_field unless project.issue_custom_fields.include?(@error_class_field)
     project.issue_custom_fields << @occurences_field unless project.issue_custom_fields.include?(@occurences_field)
     project.issue_custom_fields << @environment_field unless project.issue_custom_fields.include?(@environment_field)
   end
-  
+
 end
